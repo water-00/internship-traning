1ffaa36d028d7f6163e7d5f1ab65a6c65d2e95ac
XX469 <454951407@qq.com>
2023-07-14 16:49:11 +0800

    爬取数据的代码，加了一些注释

--- get_music_info.py
+++ get_music_info.py
@@ -7,9 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-import csv #调用数据保存文件
-import pandas as pd #用于数据输出
+import csv
+import pandas as pd
 
 import time
 import re
@@ -19,32 +18,33 @@ import random
 music_data = []
 
 
-def read_csv(path):
+def read_csv(path):  # 读取关系csv文件，获取需要爬取的作曲家列表
     names = []
     with open(path, 'r', encoding='utf-8') as file:
         # 创建CSV读取器
         reader = csv.reader(file)
         next(reader)
-        # 逐行读取CSV文件内容并打印
+        # 逐行读取CSV文件内容中的作曲家姓名信息
         for row in reader:
             names.append(row[0])
-        names = list(set(names))
+            names.append(row[1])
+        names = list(set(names))  # 去重
         return names
 
 
-def get_urls(name):
+def get_urls(name):  # 定位每个人对应的网址
     name = name.lower()
     last_name, first_name = name.split(',')
     first_name_initials = first_name.split()
     url = last_name + "_"
     for word in first_name_initials:
         url = url + word[0]
-    url_final = "https://www.classiccat.net/" + url + "/index.php"
-    print(url_final)
-    get_info(url_final)
+    url_final = "https://www.classiccat.net/" + url + "/index.php"  # 根据目标网站的url命名规则将作曲家姓名转化成对应url
+    print(url_final+" "+name)
+    get_info(url_final)  # 分别获取每一个作曲家页面的曲目信息
 
 
-def get_info(url):
+def get_info(url):  # 提取每一个作曲家页面的曲目信息（动态页面爬取）
     # 创建一个浏览器实例
     driver = webdriver.Chrome()
 
@@ -53,11 +53,12 @@ def get_info(url):
     # 网页不存在
     if "Error" in driver.title:
         print(f"url不存在: {url}")
+        error_links.append(url)
         return
     # 使用XPath定位链接文本的元素
     link_element = driver.find_element(By.XPATH, '//*[@id="div1"]/table/tbody/tr[1]/td[1]/a[4]/b')
 
-    # 点击链接
+    # 点击文本，让页面按照受欢迎程度排序
     link_element.click()
 
     # 获取操作完成后的页面源代码
@@ -65,16 +66,19 @@ def get_info(url):
     wait.until(EC.presence_of_element_located((By.TAG_NAME, "body")))  # 等待<body>元素出现
     page_source = driver.page_source
 
+    # 使用xpath方法提取对应数据
     selector = etree.HTML(page_source)
     author_name = selector.xpath('//*[@id="widthsetter"]/table[1]/tbody/tr/td[3]/h1/text()')[0].strip()
     print("作曲家：" + author_name)
     author_life = selector.xpath('//*[@id="widthsetter"]/table[1]/tbody/tr/td[3]/text()')[0].strip()
     print("生卒年：" + author_life)
+
+    # 获取该作曲家前20首热门曲目
     for i in range(20):
         # 曲名
         name_path = '//*[@id="div1"]/table/tbody/tr[{}]/td[1]/a/text()'.format(i + 2)
         # 作品数不满20
-        if selector.xpath(name_path) == []:
+        if not selector.xpath(name_path):
             break
         name = selector.xpath(name_path)[0].strip()
         print("name:" + name)
@@ -92,13 +96,13 @@ def get_info(url):
             print("duration:", duration)
         else:
             print("未找到匹配的内容")
-        # opus
+        # opus编号
         opus_path = '//*[@id="div1"]/table/tbody/tr[{}]/td[2]/text()'.format(i + 2)
-        opus = selector.xpath(opus_path)[0].strip() if selector.xpath(opus_path) != [] else ''
+        opus = selector.xpath(opus_path)[0].strip() if selector.xpath(opus_path) != [] else ''  # 网页中缺少opus，后续查找补充
         print("opus:" + opus)
         data = [rank, name, opus, version, duration, author_name, author_life]
         music_data.append(data)
-    # 不满20的继续读页面右边一列
+    # 曲目数量不满20的继续读页面右边一列
     if i != 19:
         curr_rank = i
         i = 0
@@ -106,7 +110,7 @@ def get_info(url):
             # 曲名
             name_path = '//*[@id="div2"]/table/tbody/tr[{}]/td[1]/a/text()'.format(i + 2)
             # 作品数不满20
-            if selector.xpath(name_path) == []:
+            if not selector.xpath(name_path):
                 break
             name = selector.xpath(name_path)[0].strip()
             print("name:" + name)
@@ -135,17 +139,17 @@ def get_info(url):
     driver.quit()
 
 
-def write_data_to_csv(columns, datas):
+def write_data_to_csv(columns, datas):  # 将数据写入csv
     test = pd.DataFrame(columns=columns, data=datas)  # 将数据放进表格
-    test.to_csv('music_data.csv')  # 数据存入csv,存储位置及文件名称
+    test.to_csv('music_data_new2.csv')  # 数据存入csv,存储位置及文件名称
 
 
 if __name__ == '__main__':
-    path = 'musicians_relationships_eng.csv'
+    path = 'musicians_relationships_eng.csv'  # 关系csv文件路径
     composer_list = read_csv(path)
     for composer in composer_list:
         get_urls(composer)
-    print(music_data)
+    get_info('https://www.classiccat.net/beethoven_l_van/index.php')
     print("开始写入csv。。。")
     header = ['rank', 'name', 'opus', 'versions', 'duration', 'composer_name', 'composer_life']
     write_data_to_csv(header, music_data)
